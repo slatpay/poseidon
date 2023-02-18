68a72c82a61e31b158767b7334e08264e083ba88
Gabriel Roux <65895832+gabriel-roux@users.noreply.github.com>
2023-02-18 09:19:24 -0300

    ...

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -16,5 +16,10 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/poseidon/'
+    }
   }
 }
